8d4216a6c8e5c12337208858fce3e396a98c03e0
taiga-tech <taiga.dev.n@gmail.com>
2020-10-20 15:49:36 +0900

    add PostTest

--- src/database/factories/PostFactory.php
+++ src/database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -25,7 +26,7 @@ class PostFactory extends Factory
             'text' => $this->faker->text,
             'place' => '',
             'image' => '',
-            'user_id' => rand(0,5)
+            'user_id' => rand(1,User::count())
         ];
     }
 }
