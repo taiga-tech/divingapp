be95961a06a2c02a3d0e239ba3ce728648a694dd
taiga-tech <taiga.dev.n@gmail.com>
2020-10-22 02:57:53 +0900

    error message

--- src/resources/views/partials/postForm.blade.php
+++ src/resources/views/partials/postForm.blade.php
@@ -3,10 +3,14 @@
 <div class="form-group row">
   <label for="text" class="col-md-4 col-form-label text-md-right">{{ __('Text') }}</label>
   <div class="col-md-6">
-    <textarea name="text" class="form-control">{{ $errors->has('*') ? old('text'):($post['text'] ?? '') }}</textarea>
+    <textarea name="text" class="form-control @error('text') is-invalid @enderror" required>{{ $errors->has('*') ? old('text'):($post['text'] ?? '') }}</textarea>
+
     @error('text')
-      <div class="alert alert-danger">{{ $message }}</div>
+      <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+      </span>
     @enderror
+
   </div>
 </div>
 
@@ -27,6 +31,7 @@
 <div class="form-group row mb-0">
   <div class="col-md-6 offset-md-4">
       <button type="submit" class="btn btn-outline-info">
+        {{-- textがない時押せないようにする --}}
         送信
       </button>
   </div>
