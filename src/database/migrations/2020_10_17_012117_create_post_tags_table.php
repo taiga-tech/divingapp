305f48bdcd6f213c6dda263cc8e20b351f7730bc
taiga-tech <taiga.dev.n@gmail.com>
2020-10-21 19:38:03 +0900

    テーブル名修正

--- src/database/migrations/2020_10_17_012117_create_post_tags_table.php
+++ src/database/migrations/2020_10_17_012117_create_post_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostTagTable extends Migration
+class CreatePostTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePostTagTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_tag', function (Blueprint $table) {
+        Schema::create('post_tags', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('post_id')->nullable()->constrained();
-            $table->foreignId('tag_id')->nullable()->constrained();
+            $table->foreignId('post_id')->nullable()->constrained()->onDelete('cascade');;
+            $table->foreignId('tag_id')->nullable()->constrained()->onDelete('cascade');;
             $table->timestamps();
         });
     }
