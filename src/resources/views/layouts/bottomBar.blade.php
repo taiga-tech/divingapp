3aeb34652be877f3917ab8f217b34a414dae7012
taiga-tech <taiga.dev.n@gmail.com>
2020-11-14 06:25:51 +0900

    git cache clear

--- src/resources/views/layouts/bottomBar.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="d-md-none dark:bg-gray-800" style="height: 50px">
-  <div class="d-flex h-100">
-    <a class="flex-fill d-flex align-items-center justify-content-center" href="/"><div class=""><i class="fas fa-home"></i></div></a>
-    <a class="flex-fill d-flex align-items-center justify-content-center" href="#"><div class=""><i class="fas fa-search"></i></div></a>
-  </div>
-</div>
