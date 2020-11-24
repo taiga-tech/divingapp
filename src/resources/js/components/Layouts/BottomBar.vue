9a731a5198882e151edb6064722aeaf22fb7db28
taiga-tech <taiga.dev.n@gmail.com>
2020-11-24 23:20:59 +0900

    bottombarにpost.createまでのボタン追加

--- src/resources/js/components/Layouts/BottomBar.vue
+++ src/resources/js/components/Layouts/BottomBar.vue
@@ -1,11 +1,14 @@
 <template>
 <div class="d-md-none dark:bg-gray-800" style="height: 50px">
   <div class="d-flex h-100">
-    <router-link v-on:click="home" class="flex-fill d-flex align-items-center justify-content-center" v-bind:to="{name: 'posts.index'}">
-      <div class=""><i class="fas fa-home"></i></div>
+    <router-link class="flex-fill d-flex align-items-center justify-content-center" :to="{ name: 'posts.index' }">
+      <div><i class="fas fa-home"></i></div>
     </router-link>
-    <router-link class="flex-fill d-flex align-items-center justify-content-center" v-bind:to="{ name: 'search' }">
-      <div class=""><i class="fas fa-search"></i></div>
+    <router-link v-if="isLogin" class="flex-fill d-flex align-items-center justify-content-center" :to="{ name: 'posts.create' }">
+      <div><i class="fas fa-plus"></i></div>
+    </router-link>
+    <router-link class="flex-fill d-flex align-items-center justify-content-center" :to="{ name: 'search' }">
+      <div><i class="fas fa-search"></i></div>
     </router-link>
   </div>
 </div>
@@ -13,9 +16,10 @@
 
 <script>
 export default {
-  methods: {
-    home: function event() {
+    computed: {
+    isLogin () {
+      return this.$store.getters['auth/check']
     },
-  }
+  },
 }
 </script>
