d768d1b6b36a2b9db8187b09fd211af2e8e39776
taiga-tech <taiga.dev.n@gmail.com>
2020-12-02 20:04:35 +0900

    リンク追加

--- src/resources/js/components/Post/PlaceLank.vue
+++ src/resources/js/components/Post/PlaceLank.vue
@@ -2,17 +2,37 @@
 <div class="lanking">
   <h4 class="p-2">最近投稿された地域</h4>
   <div class="lank p-2">
-    <div v-for="lank in lanking" :key="lank.id" class="d-flex p-1">
-      <p># {{ lank }}</p>
+    <div v-for="(lank, index) in lanking" :key="index" class="d-flex p-1">
+      <p v-on:click="pushSearch(index)"># {{ lank }}</p>
     </div>
   </div>
 </div>
 </template>
 
+<style lang="scss" scoped>
+.lanking {
+  p {
+    &:hover {
+      opacity:0.6;
+      cursor: pointer;
+    }
+  }
+}
+</style>
+
 <script>
 export default {
   props: {
     lanking: Array,
+  },
+  methods: {
+    async pushSearch(i) {
+      this.$router.push({
+        name: 'search.index',
+        params: { params: 'post' },
+        query: { place: this.lanking[i] }
+      })
+    }
   },
 }
 </script>
